95e9466858f149731bde75339113eaeb2148918e
Santiago <victor.lacsilva0102@gmail.com>
2022-08-07 20:50:58 -0300

    fix: correcting indentation on App.js

--- avancado/src/App.js
+++ avancado/src/App.js
@@ -7,25 +7,25 @@ import ConditionalRender from './components/ConditionalRender'
 import ShowUserName from './components/ShowUserName';
 
 function App() {
-    const [userName] = useState('Maria')
+  const [userName] = useState('Maria')
 
-    return (
-        <div className="App">
-            <h1>Avançando em React</h1>
-            {/* Imagem em public */}
-            <div>
-                <img src="/imagem.jpg" alt="Imagem sacra" />
-            </div>
-            {/* Imagem em assets */}
-            <div>
-                <img src={Inter} alt="inter" />
-            </div>
-            <ManageData />
-            <ListRender />
-            <ConditionalRender />
-            <ShowUserName name={userName} />
-        </div>
-    );
+  return (
+    <div className="App">
+      <h1>Avançando em React</h1>
+      {/* Imagem em public */}
+      <div>
+      <img src="/imagem.jpg" alt="Imagem sacra" />
+      </div>
+      {/* Imagem em assets */}
+      <div>
+      <img src={Inter} alt="inter" />
+      </div>
+      <ManageData />
+      <ListRender />
+      <ConditionalRender />
+      <ShowUserName name={userName} />
+    </div>
+  );
 }
 
 export default App;
